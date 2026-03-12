5c93ca7aeddcc6d01cb60fcb75643123066f2e97
Paul <paultraissac04012006@gmail.com>
2026-03-12 08:58:52 +0100

    changed the set of properties of EvolutionState and Level of the Weapon class

--- Assets/Code/Scripts/Inventory/Equipment/Equipment.cs
+++ Assets/Code/Scripts/Inventory/Equipment/Equipment.cs
@@ -4,11 +4,38 @@ namespace Scripts.Inventory.Equipment
     {
         public string Name { get; }
         public string Description { get; }
-        
+
         public EquipmentRarity Rarity { get; }
 
-        public int EvolutionState { get; private set; }
-        public int Level { get; private set; }
+        public int EvolutionState
+        {
+            get;
+            private set
+            {
+                if (value < 1)
+                {
+                    EvolutionState = 1;
+                }
+                else
+                {
+                    EvolutionState = value;
+                }
+            }
+        }
+        public int Level
+        {
+            get; private set
+            {
+                if (value < 1)
+                {
+                    Level = 1;
+                }
+                else
+                {
+                    Level = value;
+                }
+            }
+        }
 
         public Equipment(string name, string description, EquipmentRarity equipmentRarity)
         {
